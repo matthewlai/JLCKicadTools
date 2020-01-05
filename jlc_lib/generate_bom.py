d489a02c08161aa3e7e49a57e6fbe557badc8a08
Tobias Müller <Tobias_Mueller@twam.info>
2020-01-05 10:54:25 +0100

    Immediately create sets for part numbers/footprints

--- jlc_lib/generate_bom.py
+++ jlc_lib/generate_bom.py
@@ -42,8 +42,8 @@ def GenerateBOM(input_filename, output_filename, opts):
   num_groups_found = 0
   for group in grouped:
     refs = []
-    lcsc_part_numbers = []
-    footprints = []
+    lcsc_part_numbers = set()
+    footprints = set()
 
     for component in group:
       refs.append(component.getRef())
@@ -54,45 +54,50 @@ def GenerateBOM(input_filename, output_filename, opts):
         field_value = c.getField(field_name)
 
         if LCSC_PART_NUMBER_MATCHER.match(field_value):
-          lcsc_part_numbers.append(field_value)
+          lcsc_part_numbers.add(field_value)
         else:
-          lcsc_part_numbers.append(None)
+          lcsc_part_numbers.add(None)
 
-      footprints.append(c.getFootprint())
+      footprints.add(c.getFootprint())
 
 
     # Check part numbers for uniqueness
-    lcsc_part_numbers_set = set(lcsc_part_numbers)
-    lcsc_part_numbers_set_without_none = lcsc_part_numbers_set - set([None])
-    if len(lcsc_part_numbers_set_without_none) == 0:
+    lcsc_part_numbers_without_none = lcsc_part_numbers - set([None])
+    if len(lcsc_part_numbers_without_none) == 0:
       if opts.warn_no_partnumber:
         logging.warning("No LCSC part number found for components {}".format(",".join(refs)))
       continue
-    elif len(lcsc_part_numbers_set_without_none) != 1:
+    elif len(lcsc_part_numbers_without_none) != 1:
       logging.error("Components {components} from same group have different LCSC part numbers: {partnumbers}".format(
           components = ", ".join(refs),
-          partnumbers = ", ".join(lcsc_part_numbers_set_without_none)))
+          partnumbers = ", ".join(lcsc_part_numbers_without_none)))
       return False
-    lcsc_part_number = list(lcsc_part_numbers_set)[0]
+    lcsc_part_number = list(lcsc_part_numbers)[0]
 
-    if (not opts.assume_same_lcsc_partnumber) and (None in lcsc_part_numbers_set):
+    if (not opts.assume_same_lcsc_partnumber) and (None in lcsc_part_numbers):
       logging.error("Components {components} from same group do not all have LCSC part number {partnumber} set. Use --assume-same-lcsc-partnumber to ignore.".format(
           components = ", ".join(refs),
           partnumber = lcsc_part_number))
       return False
 
     # Check footprints for uniqueness
-    footprints_set = set(footprints)
-    footprints_set_without_none = footprints_set - set([None])
-    if len(footprints_set_without_none) == 0:
+    footprints_without_none = footprints - set([None])
+    if (len(footprints_without_none) == 0) or ((len(footprints_without_none) == 1) and (list(footprints)[0] == '')):
       logging.error("No footprint found for components {}".format(",".join(refs)))
       return False
-    if len(footprints_set_without_none) != 1:
+    if len(footprints_without_none) != 1:
       logging.error("Components {components} from same group have different foot prints: {footprints}".format(
           components = ", ".join(refs),
-          footprints = ", ".join(footprints_set_without_none)))
+          footprints = ", ".join(footprints_without_none)))
       return False
-    footprint = list(footprints_set)[0]
+    footprint = list(footprints)[0]
+
+    if (None in footprints):
+      logging.error("Components {components} from same group do not all have footprint {footprint} set.".format(
+          components = ", ".join(refs),
+          footprint = footprint))
+      return False
+
     # They don't seem to like ':' in footprint names.
     footprint = footprint[(footprint.find(':') + 1):]
 
